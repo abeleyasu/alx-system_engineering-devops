3b59a8cfd6b3a0f039cb3c21af0d33ee5f3b38e1
abeleyasu <abeleyasu98@gmail.com>
2024-09-01 02:02:22 -0700

    0-main.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,20 @@
-cript that queries the number of subscribers on a given Reddit subreddit.
+#!/usr/bin/python3
+"""
+Script that queries subscribers on a given Reddit subreddit.
 """
 
 import requests
 
-
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "python:0-subs:v1.0 (by /u/yourusername)"}
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            # Safely access the 'subscribers' key using .get() method
-            subscribers = data.get('data', {}).get('subscribers', 0)
-            return subscribers
-        else:
-            return 0
-    except requests.RequestException:
-        # Handle potential network errors
+    headers = {"User-Agent": "RedditSubscriberCounter/1.0 by Ok_Fig7822"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
 
