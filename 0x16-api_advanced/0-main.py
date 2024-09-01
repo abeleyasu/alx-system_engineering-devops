d8a470066966a2051b180803c44fb3f9b281f5af
abeleyasu <abeleyasu98@gmail.com>
2024-09-01 01:28:12 -0700

    0-main.py

--- 0x16-api_advanced/0-main.py
+++ 0x16-api_advanced/0-main.py
@@ -1,12 +1,23 @@
-#!/usr/bin/python3
+script that queries the number of subscribers on a given Reddit subreddit.
 """
-0-main
-"""
-import sys
 
-if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "python:0-subs:v1.0 (by /u/yourusername)"}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            # Safely access the 'subscribers' key using .get() method
+            subscribers = data.get('data', {}).get('subscribers', 0)
+            return subscribers
+        else:
+            return 0
+    except requests.RequestException:
+        # Handle potential network errors
+        return 0
